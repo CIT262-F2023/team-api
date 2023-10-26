901f7d60658960bff8e14b459d027b0a46080743
michael <busaniamichael@gmail.com>
2023-10-26 15:40:44 -0600

    added my name

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 app.get('/gabriel', (_, res) => { res.send('Hello Gabriel') });
+
+app.get('/michael', (_, res) => { res.send('Hello Michael') });
