b602bada3126eede72477e47c1d1da410dbc2a06
Gabriel Cánepa <gacanepa@gmail.com>
2023-11-04 22:21:35 -0300

    Update gabriel.test.js

--- test/gabriel.test.js
+++ test/gabriel.test.js
@@ -1,4 +1,4 @@
-describe('Testing mike endpoint', () => {
+describe('Testing Gabriel endpoint', () => {
     test('Check if the response matches the expected name', async () => {
       const response = await fetch('http://localhost:3000/gabriel');
       const name = await response.text();
